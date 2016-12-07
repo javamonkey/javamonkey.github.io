482f74a380ebff61e7f4538854876aee637705be
ydq <10176523@qq.com>
2016-12-07 22:48:02 +0800

    文档手册页面的重新改造，以后仅需维护guide/beetl.md和guide/beetlsql.md文件

---
+++ guide/static/beetldoc.js
@@ -0,0 +1,84 @@
+var guide = new Vue({
+	el:'#guide',
+	data:{type:'',doc:'',nav:[],search:''},
+	computed:{
+		searchlist:function(){
+			return localSearch(this.search,this.nav,[]);
+		}
+	},
+	components:{
+		'navmenu':{
+			name:'navmenu',
+			props: ['list'],
+			template: '<ul v-if="list.length"><li v-for="n of list" :title="n.text"><a :href="n.href" @click="guide.jump" v-html="n.text"></a><navmenu  v-if="list.length" :list="n.child"></navmenu></li></ul>'
+		}
+	},
+	methods:{
+		jump:function(e){
+			this.search = '';
+			e.preventDefault();
+			document.body.scrollTop = document.querySelector(e.target.getAttribute('href')).offsetTop;
+		}
+	}
+})
+var render = new marked.Renderer(),toc;
+render.heading = function(text,level){
+	var reg = /^((\d+_)+).*/.exec(text.replace(/\./g,'_')),tag = '<h'+level;
+	if(reg){
+		tag += ' id="beetl_'+reg[1]+'" class="_anchor">';
+		var current = {href:'#beetl_'+reg[1],text:text,child:[]}
+		if(level == 3){
+			toc.push(current)
+		}else if(level == 4){
+			toc[toc.length -1].child.push(current)
+		}else if(level ==5){
+			toc[toc.length -1].child[toc[toc.length -1].child.length-1].child.push(current)
+		}
+	}else{
+		tag += '>';
+	}
+	tag += text+'</h'+level+'>'
+	return tag
+}
+function loadDoc(){
+	var docs = ['beetl','beetlsql'],doc = location.hash.substr(1)
+	if(docs.indexOf(doc)<0) doc = 'beetl';//所有非法的参数都归为beetl
+	toc=[];
+	document.body.scrollTop = 0
+	NProgress.start();
+	$.get(doc+'.md?_='+new Date().getDay(),function(md){
+		marked(md,
+			{renderer:render,highlight:function(code,lang){return hljs.highlightAuto(code).value}},
+			function(err,html){
+				guide.search='';
+				guide.type = doc;
+				guide.doc = html;
+				Vue.set(guide,'nav',toc);
+				NProgress.done();
+			}
+		)
+	})
+}
+$(window).on('scroll',_.throttle(function(){
+	var bdScrollTop = document.body.scrollTop;
+	$('#nav .active').removeClass('active')
+	$('._anchor').each(function(i,n){
+		if(bdScrollTop<=this.offsetTop){
+			var current = $('#nav a[href="#'+this.id+'"]').addClass('active')
+			return false;
+		}
+	})
+},300)).on('hashchange',loadDoc)
+function localSearch(search,nav,arr){
+	try{
+		var reg = new RegExp(search,'i')
+		nav.forEach(function(n){
+			if(n.text.match(reg)) arr.push(n);
+			if(n.child.length) localSearch(search,n.child,arr);
+		})
+	}catch(e){
+		
+	}
+	return arr;
+}
+loadDoc();
